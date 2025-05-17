017355cd28d0ab54ae00b66b40bbfe9f4d8eb4b0
saad19987 <saadmushtaq@ragioneer.com>
2025-05-17 16:31:40 +0500

    fixed the rerouting issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,25 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { DecodedToken, verifyToken } from "@/lib/auth";
-import { JwtPayload } from "jsonwebtoken";
-import { jwtDecode } from "jwt-decode";
-
-interface ExtendedJwtPayload extends JwtPayload {
-  email?: string;
-}
 
 export default function Home() {
   const token = cookies().get("token")?.value;
-  const user = token ? (verifyToken(token) as ExtendedJwtPayload) : null;
-  const decoded = token ? jwtDecode<DecodedToken>(token) : null;
 
-  if (user && decoded?.role === "admin") {
-    redirect("/admin/upload-medical-files");
-  }
-
-  if (!user || !user.email) {
+  if (!token) {
     redirect("/login");
   }
 
-  if (user && decoded?.role !== "admin") {
-    redirect("/ask-me-anything");
+  let decoded: DecodedToken;
+
+  try {
+    decoded = verifyToken(token) as DecodedToken;
+  } catch (err) {
+    redirect("/login");
   }
 
-  return null;
+  if (decoded.role === "admin") {
+    redirect("/admin/upload-medical-files");
+  }
+
+  redirect("/ask-me-anything");
 }
